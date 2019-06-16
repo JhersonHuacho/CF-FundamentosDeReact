3000bf4d71d317ebe40cd9433de8076f82211799
Jherson <jhersonhuacho@gmail.com>
2019-06-16 11:25:35 -0500

    01 - Mi primer componente

--- fundamentos-react/src/App.js
+++ fundamentos-react/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Componente Funcional : Representación de un componente a travez de una función
+function MiComponenteFuncional() {
+  return <p>Hola Mundo desde una función</p>;
 }
 
+// Componente de clase : Representación de un componente a travez de clases.
+class MiComponenteDeClase extends Component{
+  render() {
+    return <p>Hola mundo desde una clase</p>;
+  }    
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <MiComponenteFuncional />
+      <MiComponenteDeClase />
+    </div>
+    );
+  }
+}
+
+//export default MiComponenteFuncional;
 export default App;
