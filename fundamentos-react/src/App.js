22bc92024420b55ffbceaa4905abbf0fcf4d0386
Francisco Jherson Huacho Inga <jhersonhuacho@gmail.com>
2019-07-07 11:33:31 -0500

    02 - Props

--- fundamentos-react/src/App.js
+++ fundamentos-react/src/App.js
@@ -14,6 +14,48 @@ class MiComponenteDeClase extends Component{
   }    
 }
 
+// 02. Props
+//----------------------------
+/* En un sistema debemos procurar evitar desarrollar elementos que dependan entre ellos.
+ * Es decir debemos desarrollar elementos independientes entre si, que no exista cohesión entre los
+ * elementos que forman el sistema.
+ * El desarrollo basado en componentes es especialmente util en estos casos, aqui buscamos que cada componente sea
+ * independiente entre si.
+ * Cada componente debe buscar ser su propio mundo y no debera usar información externa a el mismo.
+ * 
+ * Props, en terminos tecnicos las props en REACT son propiedades de creación del componente. Es decir, información que
+ * establecemos para un componente cuando lo creamos. Esto es la forma a trabez de lo cual un componente padre pasa información
+ * hacia los componentes hijos.
+ * 
+ * Toda la información que el componente necesita del exterior la tiene que recibir via PROPS.
+ * Los PROPS son readonly, es decir, un componente no puede modificar sus propias PROPS, sola las puede leer.
+ * Cuando el valor que se pasa via PROPS a un componente, se actualiza o se modifica. El componente se actualiza automaticamente.
+ * 
+ * Las PROPS es uno de los conceptos fundamentales del trabajo con REACT porque es a travez de ellos que pasamos información de
+ * inicialización a nuestros componentes.
+ */
+let nombre = "Francisco";
+function A() {
+  return <p>Hola {nombre} Componente A</p>;
+}
+function B() {
+  return <p>Hola {nombre} Componente B</p>;
+}
+// props en un componente funcional
+// En un componente funcional, las props se reciben como el unico argumento de la función
+function C(props) {
+  return <p>Hola {props.nombre} Componente C</p>;
+}
+function D(props) {
+  return <p>Hola {props.nombre} Componente D</p>;
+}
+// props en un componente de clases
+class Eclass extends Component{
+  render() {
+    return <p>Hola mundo desde una clase {this.props.nombre}</p>;
+  }    
+}
+//----------FIN 02---------------------------------
 // function App() {
 //   return (
 //     <div className="App">
@@ -37,10 +79,16 @@ class MiComponenteDeClase extends Component{
 
 class App extends Component {
   render() {
+    let nombreProp = "Prop Francisco";
     return (
       <div>
       <MiComponenteFuncional />
       <MiComponenteDeClase />
+      <A />
+      <B />
+      <C nombre={nombreProp} />
+      <D nombre="Props Jherson" />
+      <Eclass nombre={nombreProp}/>
     </div>
     );
   }
